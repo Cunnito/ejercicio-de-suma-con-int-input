e6d0038d7ca508bacb1dc341f81449961d193aa7
Cunnito <103428973+Cunnito@users.noreply.github.com>
2024-03-20 09:01:51 -0300

    Add files via upload

---
+++ ejercicio de integracion con input.py
@@ -0,0 +1,9 @@
+#variables para almacenar los datos requeridos
+numero1 = int(input('ingrese un numero: '))
+numero2 = int(input('ingrese un numero: '))
+
+#variable para almacenar el resultado de la suma
+suma = numero1 + numero2
+
+#mensaje en pantalla
+print('El resultado de su suma', numero1, '+', numero2, 'es', suma)
